ab8952a5fdde948740b2126d2dd9a0c231fc3fc4
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-08-26 10:40:47 +0200

    Test Git Hook

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API pour FATBOAR' })
+    response.json({ info: 'Node.js, Express, and Postgres API pour FATBOAR JEU CONCOURS' })
 })
 
 app.listen(port, () => {
