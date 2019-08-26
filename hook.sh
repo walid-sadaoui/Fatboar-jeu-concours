7b51d832fb198d6010bc12f1604a1a66a4630103
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-08-26 11:47:40 +0200

    Test Git Hook

--- hook.sh
+++ hook.sh
@@ -6,7 +6,6 @@ echo ${apikey}
 # branch=$(git rev-parse --symbolic --abbrev-ref $1)
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-
 token=$(curl -u "gogs:117be57682d7fe7cfc485b7f306da8e174" 'https://jenkins.fatboar.site/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
 
 echo "Branch : " ${branch}
