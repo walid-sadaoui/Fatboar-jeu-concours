1dddf64a872a9f1ee96d2814bb340f59ceb02977
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-08-26 11:42:24 +0200

    Test Git Hook

--- hook.sh
+++ hook.sh
@@ -9,4 +9,4 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 token=$(curl -u "gogs:117be57682d7fe7cfc485b7f306da8e174" 'https://jenkins.fatboar.site/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
 
 echo "Branch : " ${branch}
-# curl -X POST -u "gogs:${apikey}" -H ".crumb:${token}" https://jenkins.fatboar.site/job/${projet}/job/api-docker-config/build
+curl -X POST -u "gogs:${apikey}" -H ".crumb:${token}" https://jenkins.fatboar.site/job/${projet}/job/${branch}/build
