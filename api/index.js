64c798e4e8b4d9aa363657c2585c3ab56413bc3f
METCHUM Sonia <smetchum@OPEN.LIAE.LISI>
2019-10-14 17:25:58 +0200

    back

--- api/index.js
+++ api/index.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const helmet = require('helmet')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
 app.use(helmet())
 app.use(bodyParser.json())
+app.use(cors())
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: false,
+    })
 )
+var Users = require('./routes/Users')
+app.use('/users', Users)
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API pour FATBOAR JEU CONCOURS' })
---
+++ api/models/Users.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize')
+const db = require('../database/db')
+
+module.exports = db.sequelize.define(
+    'user', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: Sequelize.STRING
+        },
+        Last_name: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        },
+        created: {
+            type: Sequelize.STRING
+        },
+        created: {
+            type: Sequelize.STRING
+        },
+        created: {
+            type: Sequelize.DATE,
+            defaultValue: Sequelize.Now
+        }
+    }, {
+        timestamps: false
+    }
+
+)
