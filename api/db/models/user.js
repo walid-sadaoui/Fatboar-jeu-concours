7664a11ee356e9d2fff19c341b529d57688bacdf
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-10-22 19:15:07 +0200

    fix: 🐛 conflicts setProjectBase

--- api/db/models/user.js
+++ api/db/models/user.js
@@ -1,10 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    username: DataTypes.STRING
-  }, {});
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    first_name: {
+        type: DataTypes.STRING
+    },
+    Last_name: {
+        type: DataTypes.STRING
+    },
+    email: {
+        type: DataTypes.STRING
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    created: {
+        type: DataTypes.STRING
+    },
+
+}, {
+  timestamps: false
+});
   User.associate = function(models) {
     // associations can be defined here
   };
