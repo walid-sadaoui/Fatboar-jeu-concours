154f2a6f8b677f760fb84814f24adafd1b1bd0a1
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-11-01 10:26:40 +0100

    user model

--- api/db/models/user.js
+++ api/db/models/user.js
@@ -1,28 +1,72 @@
 'use strict';
+const bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+    idUser: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
-    first_name: {
-        type: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING
     },
-    last_name: {
-        type: DataTypes.STRING
+    lastName: {
+      type: DataTypes.STRING
     },
-    email: {
-        type: DataTypes.STRING
+    userName: {
+      type: DataTypes.STRING
     },
     password: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING // 1 chiffre, 1 minusule, 1 majuscule, 8 caractères
     },
-}, {
-  timestamps: false
-});
-  User.associate = function(models) {
+    email: {
+      type: DataTypes.STRING, // check format mail
+      validate: {
+        isEmail: true,
+      },
+    },
+    isConnected: {
+      type: DataTypes.BOOLEAN // default: false
+    },
+    role: {
+      type: DataTypes.ENUM,
+      values: ['CLIENT', 'EMPLOYE', 'RESPONSABLE']
+    },
+    state: {
+      type: DataTypes.ENUM,
+      values: ['EN LIGNE', 'HORS LIGNE']
+    },
+  }, {
+    timestamps: false
+  });
+  User.associate = function (models) {
     // associations can be defined here
+    User.hasMany(models.Ticket, {
+      as: 'tickets'
+    })
   };
+
+  User.beforeCreate((user, options, fn) => {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        console.log('Error while generating bcrypt salt.');
+        console.log(err);
+        fn(err, null);
+        return;
+      }
+
+      bcrypt.hash(user.password, salt, null, (err, hash) => {
+        if (err) {
+          console.log('Error while generating bcrypt hash.');
+          console.log(err);
+          fn(err, null);
+          return;
+        }
+        user.password = hash;
+        fn(null, user);
+        return user;
+      });
+    });
+  });
   return User;
 };
