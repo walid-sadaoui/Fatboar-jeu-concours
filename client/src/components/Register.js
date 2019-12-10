306d9b292ada1dec378dbf2616fd39cda9594c12
Charles <atanguicharles@gmail.com>
2019-12-10 14:45:06 +0100

    MAJ 10dec - styling 2

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -11,12 +11,21 @@ export default class Dashboard extends Component {
             <div className="register-page" style={{minheight: 5}}>
                 <div className="register-box">
                     <div className="register-logo">
-                    <img className="LogoConnexion wow fadeInUp" src={logo} alt="" width="45%"/>  
+                    
+                    <img src={TitreInscription} alt="fatboar-inscription" width="100%" class="img-fluid"/>
                     </div>
-                    <div className="card">
+                    <div className="card habillage-form">
                         <div className="card-body register-card-body">
-                            <p className="login-box-msg">S'enregistrer</p>
+                            {/* <p className="login-box-msg">S'enregistrer</p> */}
                             <form action="../../index.html" method="post">
+                            <div className="input-group mb-3">
+                                    <input type="number" className="form-control" placeholder="N° du Ticket (optionnel) " />
+                                        <div className="input-group-append">
+                                            <div className="input-group-text">
+                                                <span className="fas fa-ticket-alt"></span>
+                                            </div>
+                                        </div>
+                                </div>
                                 <div className="input-group mb-3">
                                     <input type="text" className="form-control" placeholder="Nom" />
                                         <div className="input-group-append">
@@ -50,35 +59,23 @@ export default class Dashboard extends Component {
                                         </div>
                                 </div>
                                 <div className="row">
-                                    <div className="col-8">
-                                        <div className="icheck-primary">
-                                            <input type="checkbox" id="agreeTerms" name="terms" value="agree" />
-                                                <label htmlFor="agreeTerms">
-                                                    Je suis d'accord <a href="#">terms</a>
-                                                </label>
-                                        </div>
-                                    </div>
-
                                     <div className="col-4">
-                                        <button type="submit" className="btn btn-primary btn-block">Créer</button>
+                                        <button type="submit" className="btn btn-primary btn-block">S'inscrire</button>
                                     </div>
 
                                 </div>
                             </form>
 
                             <div className="social-auth-links text-center">
-                                <p>- OU -</p>
+                                <p className="centre">- OU -</p>
                                 <a href="#" className="btn btn-block btn-primary">
-                                    <i className="fab fa-facebook mr-2"></i>
-                                    S'enregistrer par Facebook
+                                <i className="fab fa-facebook mr-2"></i> Inscription par Facebook
                                 </a>
-                                <a href="#" className="btn btn-block btn-dark">
-                                    <i className="fab fa-github mr-2"></i>
-                                    S'enregistrer par Github
+                                <a href="#" className="btn btn-block btn-danger">
+                                <i className="fab fa-google mr-2"></i> Inscription par Google
                                 </a>
                             </div>
-
-                            <NavLink to="/login" className="text-center">Je suis déjà un membre</NavLink>
+                            <NavLink to="/login"><h6 className="centre">Je suis déjà un membre</h6></NavLink>
                         </div>
 
                     </div>
