53788d044ebfe9c639d9e27680212c77c5f2844b
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-12-17 08:40:30 +0100

    cors pb

--- api/app.js
+++ api/app.js
@@ -23,7 +23,7 @@ app.use(
 )
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*.fatboar.site");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
