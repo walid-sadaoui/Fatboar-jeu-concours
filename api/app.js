a1cbe6dca415a04f4a6476ae454aa0686203350e
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-12-25 09:00:06 +0100

    cors issue

--- api/app.js
+++ api/app.js
@@ -15,7 +15,12 @@ const indexRouter = require('./routes/index');
 
 app.use(helmet())
 app.use(bodyParser.json())
-app.use(cors())
+// app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 // app.use(
 //     '/',
 //     proxy({ target: API_URL, changeOrigin: true })
