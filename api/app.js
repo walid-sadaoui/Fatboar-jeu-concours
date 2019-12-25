848ecf12b5a2441f6675ac9e19c2f5f455dea8c6
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-12-25 09:37:07 +0100

    cors issue again

--- api/app.js
+++ api/app.js
@@ -18,7 +18,8 @@ app.use(bodyParser.json())
 // app.use(cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
 // app.use(
