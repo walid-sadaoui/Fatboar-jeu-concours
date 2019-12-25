1b87333fa94fdf950a23a94b0767154cd5bf1063
Walid SADAOUI <walidsadaoui@hotmail.fr>
2019-12-25 09:53:59 +0100

    cors issue again

--- api/app.js
+++ api/app.js
@@ -15,13 +15,13 @@ const indexRouter = require('./routes/index');
 
 app.use(helmet())
 app.use(bodyParser.json())
-// app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+app.use(cors())
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     next();
+//   });
 // app.use(
 //     '/',
 //     proxy({ target: API_URL, changeOrigin: true })
